16983efec39817a80a93a0283046db64d57da4f3
carlosaw <carlosfreevg@gmail.com>
2023-06-23 13:31:42 -0400

    add_column

--- database/migrations/1687483124725_users.ts
+++ database/migrations/1687483124725_users.ts
@@ -6,8 +6,9 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name')
-      table.string('email')     
+      table.string('username').unique().notNullable()
+      table.string('email').unique().notNullable()
+      table.boolean('is_admin').defaultTo(0)     
       table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
       table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
     })
